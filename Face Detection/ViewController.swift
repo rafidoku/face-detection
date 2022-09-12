e1c5d072a2756c0b248f48189b6dc395afe51d02
DOKU IT <104545377+rafidoku@users.noreply.github.com>
2022-09-12 14:34:38 +0700

    finalize

--- Face Detection/ViewController.swift
+++ Face Detection/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         accuracyLabel.text = "Scanning . . ."
         let captureSession = AVCaptureSession()
 
-        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
             return
         }
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {
@@ -32,6 +32,7 @@ class ViewController: UIViewController {
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
+        view.bringSubviewToFront(accuracyLabel)
         
         let dataOutput = AVCaptureVideoDataOutput()
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
@@ -51,35 +52,24 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
         let imageSize = CVImageBufferGetDisplaySize(pixelBuffer)
         var actualRect = CGRect()
         if let faces = faces {
+            if faces.count == 0 {
+                DispatchQueue.main.async {
+                    actualRect = CGRect()
+                    self.accuracyLabel.text = "Arahkan kamera ke wajah anda"
+                }
+            }
             for face in faces {
                 actualRect = CGRect(x: face.bounds.origin.x, y: imageSize.height - face.bounds.origin.y - face.bounds.height, width: face.bounds.width, height: face.bounds.height)
-                print(actualRect)
-                
+                DispatchQueue.main.async {
+                    self.accuracyLabel.text = "Wajah terdeteksi"
+//                    let bound = UIView(frame: actualRect)
+//                    bound.backgroundColor = .yellow
+//                    self.captureView.addSubview(bound)
+//                    self.captureView.bringSubviewToFront(bound)
+                }
             }
-            DispatchQueue.main.async {
-                let bound = UIView(frame: actualRect)
-                bound.backgroundColor = .yellow
-                self.view.addSubview(bound)
-            }
+        } else {
+            self.accuracyLabel.text = "Arahkan kamera ke wajah anda"
         }
-//        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
-//            return
-//        }
-//        var identifier = VNClassificationObservation()
-//        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
-//
-//            guard let results = finishedReq.results as? [VNClassificationObservation] else {
-//                return
-//            }
-//
-//            guard let firstObservation = results.first else {
-//                return
-//            }
-//            DispatchQueue.main.async {
-//                self.accuracyLabel.text = "\(firstObservation.identifier), \(firstObservation.confidence)"
-//            }
-//
-//        }
-//        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
 }
