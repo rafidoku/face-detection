d6b7bc038fac909dd9dadf2fb831465ca7d4dba5
DOKU IT <104545377+rafidoku@users.noreply.github.com>
2022-09-13 10:12:02 +0700

    Spike anti simulator
    Spike anti simulator

--- Face Detection/AppDelegate.swift
+++ Face Detection/AppDelegate.swift
@@ -15,8 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
+        #if targetEnvironment(simulator)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UIStoryboard(name: "SimulatorDetectedStoryboard", bundle: nil).instantiateInitialViewController()
+        #else
         window?.makeKeyAndVisible()
         window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        #endif
         return true
     }
 
---
+++ Face Detection/SimulatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SimulatorViewController.swift
+//  Face Detection
+//
+//  Created by DOKU IT on 13/09/22.
+//
+
+import UIKit
+
+class SimulatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
